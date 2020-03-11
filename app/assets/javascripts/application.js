cd4025234c60e8fe741b854f86f08d9d5227449e
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-11 22:11:51 +0530

    resouces related changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,3 @@
 //= require angular
+//= require angular-resource
 //= require_tree .
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+1.upto(10) do |i|
+  Event.create(name: "Event #{i}",
+               description: "It's sample event with number #{i}",
+               event_date: Date.today + rand(3).months,
+               place: "Random place number #{i}")
+end
