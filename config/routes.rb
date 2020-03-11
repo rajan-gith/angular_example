7d4b2ddb0b3cafda2f342197b4674dbc1cfcec05
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-11 22:13:23 +0530

    routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'dashboard#index'
+  namespace :api do
+    resources :events, only: [:index, :create]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
