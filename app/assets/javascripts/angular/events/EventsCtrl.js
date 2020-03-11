0b12abf08eb78a71f18faad546298da8e2826fcf
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-11 18:23:56 +0530

    first commit with ng-controller and ng-submit

---
+++ app/assets/javascripts/angular/events/EventsCtrl.js
@@ -0,0 +1,39 @@
+var app = angular.module('angularExample');
+
+app.controller('EventsCtrl', ['$scope', function($scope) {
+  $scope.events = [
+    // { name: 'Event 1', event_date: '01/10/2016', description: 'Description #1', place: 'Place #1'},
+    // { name: 'Event 2', event_date: '02/10/2016', description: 'Description #2', place: 'Place #2'},
+    // { name: 'Event 3', event_date: '03/10/2016', description: 'Description #3', place: 'Place #3'},
+    // { name: 'Event 4', event_date: '04/10/2016', description: 'Description #4', place: 'Place #4'},
+    // { name: 'Event 5', event_date: '05/10/2016', description: 'Description #5', place: 'Place #5'},
+  ];
+
+  $scope.addEvent = function() {
+    console.log("function started");
+    if (!valid()){
+      console.log("data not valid");
+
+      return false;
+    }
+    console.log("data valid");
+  //
+    $scope.events.push({
+      name: $scope.event.name,
+      event_date: $scope.event.event_date,
+      description: $scope.event.description,
+      place: $scope.event.place
+    });
+
+    $scope.event.name = '';
+    $scope.event.description = '';
+    $scope.event.event_date = '';
+    $scope.event.place = '';
+
+  }
+  valid = function() {
+    return !!$scope.event && !!$scope.event.name && !!$scope.event.event_date && !!$scope.event.description && !!$scope.event.place;
+  }
+
+
+}]);
